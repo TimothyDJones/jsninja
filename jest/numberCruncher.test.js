0fec956c172ce7b89208dbbd27dafd239f5a3399
Tim Jones <tdjones74021@yahoo.com>
2017-10-16 17:08:56 -0500

    Implement isPrime() method to make tests pass!

--- jest/numberCruncher.test.js
+++ jest/numberCruncher.test.js
@@ -10,6 +10,11 @@ function factorsOf(n) {
 	return factors.sort((a, b) => (a - b));
 }
 
+function isPrime(n) {
+'use strict';
+	return (factorsOf(n).length === 2);
+}
+
 test('factors of 12', () => {
 	expect(factorsOf(12)).toEqual([1, 2, 3, 4, 6, 12]);
 });
